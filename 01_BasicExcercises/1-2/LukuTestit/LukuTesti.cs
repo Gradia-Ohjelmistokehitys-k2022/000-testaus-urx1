05982d1cb4ca9fe309516468c3ff32efbbce9e89
urx1 <121085632+urx1@users.noreply.github.com>
2024-02-05 18:43:23 +0200

    continued work on 1

--- 01_BasicExcercises/1-2/LukuTestit/LukuTesti.cs
+++ 01_BasicExcercises/1-2/LukuTestit/LukuTesti.cs
@@ -11,14 +11,62 @@ namespace LukuTestit
             int alkuLuku = 5;
             int vahennettavaLuku = 4;
             int odotettuLuku = 1;
+
             Luvut testiLuvut = new Luvut(alkuLuku, vahennettavaLuku);
 
             testiLuvut.Vahenna();
 
             int testinTulos = testiLuvut.miinusTulos;
+
             Assert.AreEqual(odotettuLuku, testinTulos, 0.001, "Virhe vähennyksessä, luvut eivät tuota odotettua lopputulosta.");
         }
 
-    }
+        [TestMethod]
+        public void VahennettavaLukuEiNolla()
+        {
+            int alkuLuku = 5;
+            int vahennettavaLuku = 4;
+            int odotettuLuku = 1;
 
+            Luvut testiLuvut = new Luvut(alkuLuku, vahennettavaLuku);
+
+            testiLuvut.Vahenna();
+
+            int testinTulos = testiLuvut.miinusTulos;
+
+            if (odotettuLuku < alkuLuku || vahennettavaLuku == 0)
+            {
+                return;
+            }
+            else
+            {
+                Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => testinTulos);
+            }
+
+        }
+
+        [TestMethod]
+        public void lukuVahentaa()
+        {
+            int alkuLuku = 5;
+            int vahennettavaLuku = 4;
+            int odotettuLuku = 1;
+
+            Luvut testiLuvut = new Luvut(alkuLuku, vahennettavaLuku);
+
+            testiLuvut.Vahenna();
+
+            int testinTulos = testiLuvut.miinusTulos;
+
+            if (testinTulos > alkuLuku)
+            {
+                Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => testinTulos);
+            }
+            else
+            {
+                return;
+            }
+
+        }
+    }
 }
