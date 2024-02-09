66c74aeffefe3b81cdd924d94718cd3875106638
urx1 <121085632+urx1@users.noreply.github.com>
2024-02-09 03:43:21 +0200

    Actual progress on 02

---
+++ 02_BankAssignment/2_Refaktorointi/BankAccount.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankCustomerNS;
+
+namespace BankAccountNS
+{
+    public class BankAccount
+    {
+        public int m_accountId;
+        public string m_accountType;
+        public BankCustomer m_accountOwner;
+        public double m_balance;
+        /*
+        public override string ToString()
+        {
+            return "BankAccount: " + m_accountOwner + " Balance: " + m_balance;
+        }
+        */
+
+        public BankAccount() { }
+
+        public BankAccount(int accountId, string accountType, BankCustomer accountOwner, double balance)
+        {
+            m_accountId = accountId;
+            m_accountType = accountType;
+            m_accountOwner = accountOwner;
+            m_balance = balance;
+        }
+
+        public double Balance
+        {
+            get { return m_balance; }
+        }
+
+        public static void CreateAccount(int accountId, string accountType, BankCustomer accountOwner, double balance) 
+        { 
+            BankAccount account = new BankAccount(accountId, accountType, accountOwner, balance);
+            accountOwner.m_accounts.Add(account);
+        }
+
+        public static void ShowAccounts(BankCustomer accountOwner)
+        {
+            foreach (BankAccount m_account in accountOwner.Accounts)
+            {
+                Console.WriteLine(accountOwner.m_customerName + " " + m_account.m_accountId + " " + m_account.m_balance);
+            }
+        }
+
+        public void Debit(double amount)
+        {
+            if (amount > m_balance)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            m_balance -= amount;
+        }
+
+        public void Credit(double amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount");
+            }
+
+            m_balance -= amount;
+        }
+    }
+}
---
+++ 02_BankAssignment/2_Refaktorointi/BankCustomer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using BankAccountNS;
+
+namespace BankCustomerNS
+{
+
+    //Tee BankCustomerista pääluokka
+    //  -propertynä name
+    //  -propertyna accounts (array/list?)
+    //Alaluokaksi BankAccount
+    //  -propertyna balance
+    //Luo BankCustomerille metodi AddAccount
+    //   -Lisää uuden objektin BankAccount propertyyn accounts
+
+    public class BankCustomer
+    {
+        public string m_customerName;
+        public List<BankAccount> m_accounts;
+
+        public override string ToString()
+        {
+            return "BankCustomer: " + m_customerName + " " + String.Join(" ", m_accounts);
+        }
+
+        private BankCustomer() { }
+
+
+        public string CustomerName
+        {
+            get { return m_customerName; }
+        }
+
+        public List<BankAccount> Accounts
+        {
+            get { return m_accounts; } 
+        }
+
+        public static void Main()
+        {
+            var cust1 = new BankCustomer
+            {
+                m_customerName = "Mr. Bryan Walton", 
+                m_accounts = new List<BankAccount>() 
+            };
+            BankAccount.CreateAccount(1, "Debit", cust1, 234.2);
+            BankAccount.CreateAccount(2, "Credit", cust1, 2345.2);
+
+            //Console.WriteLine(cust1.m_accounts.Count
+            //Console.WriteLine("Current balance is ${0}", ba.Balance);
+        }
+    }
+}
